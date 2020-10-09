07fbec772f57579a4b46ffc019ce4d7a2dfb0217
Frederik Kristensen <fred009a@edu.easj.dk>
2020-10-09 10:06:48 +0200

    Start up

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,23 @@
+interface ibook {
+    id: number;
+    Price: number;
+    Title: string;
+    Author: string;
+}
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        id: "",
+        price: "",
+        title: "",
+        author: "",
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
-        }
+        
+        
     }
 })
